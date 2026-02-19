17eccd05c1e71e3f2f4f074acabd5d69cb3e458b
zakina <zakina.rostand@gmail.com>
2026-02-19 16:02:40 +0100

    enregistrement de fichiers non sauvés dans branche show-chantier-kpi

---
+++ src/Dto/Chantier/ChantierKpiOutput.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Dto\Chantier;
+
+use App\Dto\Chantier\ChantierPosteKpiOutput;
+
+class ChantierKpiOutput
+{
+    public int $id;
+    public ?string $nomClient = null;
+    public ?string $ville = null;
+    public ?\DateTime $dateDemarrage = null;
+    public ?\DateTime $dateReception = null;
+    public ?float $coefficient = null;
+    public ?string $equipe = null;
+
+    // Totaux chantier (par défaut 0)
+    public float $totalHT = 0.0;
+    public float $totalTTC = 0.0;
+    public float $totalFournitures = 0.0;
+    public float $totalNbJoursTravailles = 0.0; // ou int si c’est un entier
+    public int $totalNbTrajets = 0;
+    public float $totalPrestataire = 0.0;
+    public float $totalMainOeuvre = 0.0;
+    public float $totalMainOeuvreSansTransport = 0.0;
+    public float $totalTransport = 0.0;
+    public float $totalCout = 0.0;
+    public float $marge = 0.0;
+    public float $tauxMarge = 0.0;
+
+    /** @var ChantierPosteKpiOutput[] */
+    public array $postes = [];
+}
---
+++ src/Dto/Chantier/ChantierPosteKpiOutput.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Dto\Chantier;
+
+class ChantierPosteKpiOutput
+{
+    public int $id;
+    public string $libelle;
+    public ?float $montantHT = null;
+    public ?float $montantTTC = null;
+    public ?float $montantFournitures = null;
+    public ?float $nbJoursTravailles = null;
+    public ?int $nbTrajets = null;
+    public ?float $montantPrestataire = null;
+    public ?float $montantMainOeuvre = null;
+    public ?float $montantCoutPoste= null;
+    public ?float $margePoste = null;
+    public ?float $tauxMargePoste = null;
+    /* @var EtapeOutput[] */
+   //public array $etapes = [];
+}

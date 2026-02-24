881ea80641f2f2af18cfa9deab5c704b56a702b7
zakina <zakina.rostand@gmail.com>
2026-02-24 22:13:06 +0100

    noms des endpoints renommés

--- src/Controller/Api/ChantierController.php
+++ src/Controller/Api/ChantierController.php
@@ -8,33 +8,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api2/chantiers')]
+#[Route('/api/v1/chantiers', name: 'api_v1_chantiers_')]
 class ChantierController extends AbstractController
 {
     public function __construct(
         private readonly ChantierService $chantierService
     ) {}
 
-    #[Route('/list', name: 'chantiers_list', methods: ['GET'])]
+    #[Route('', name: 'list', methods: ['GET'])]
     public function list(): JsonResponse
     {
         return $this->json($this->chantierService->list());
     }
 
-    #[Route('/show_overview/{id}', name: 'chantier_show_overview', methods: ['GET'])]
-    public function showOverview(Chantier $chantier): JsonResponse
+    #[Route('/{id<\d+>}/overview', name: 'overview', methods: ['GET'])]
+    public function overview(Chantier $chantier): JsonResponse
     {
         return $this->json($this->chantierService->showOverview($chantier));
     }
 
-    #[Route('/show_kpi/{id}', name: 'chantier_show_kpi', methods: ['GET'])]
-    public function showKpi(Chantier $chantier): JsonResponse
+    #[Route('/{id<\d+>}/kpi', name: 'kpi', methods: ['GET'])]
+    public function kpi(Chantier $chantier): JsonResponse
     {
         return $this->json($this->chantierService->showKpi($chantier));
     }
 
-    #[Route('/show_suivi/{id}', name: 'chantier_suivi', methods: ['GET'])]
-    public function showSuivi(Chantier $chantier): JsonResponse
+    #[Route('/{id<\d+>}/suivi', name: 'suivi', methods: ['GET'])]
+    public function suivi(Chantier $chantier): JsonResponse
     {
         return $this->json($this->chantierService->showSuivi($chantier));
     }
